651e99b122868174db7be76bdef3bca90b63a173
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-10-29 10:01:43 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "gw_name" {}
+
+variable "sec_domain_name" {}
+
+variable "azure_account_name" {}
+
+variable "avtx_transit_gw" {}
+
+variable "azure_region" {}
+
+variable "azure_cidr" {}
+
+variable "avtx_gw_size" { default = "Standard_B1ms" }
+
+variable "avtx_gw_ha" {default = false}
+
+variable "hpe" { default = false}
