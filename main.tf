0121ea40b8b14ddf4155c3a0364c56a19511d3dc
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-05 16:20:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
 ########## Provider 
 provider "aviatrix" {
   username     = "admin"
-  password      = var.example
-  controller_ip = "35.171.31.227"
+  password      = var.ctrl_password
+  controller_ip = "18.157.190.93"
   version       = "2.17.0"
 }
 
 
 #######################################################################
-# Azure Vnet
+# VPC/Vnet
 #
-resource "aviatrix_vpc" "azure_vnet" {
-  cloud_type           = 8
-  account_name         = var.azure_account_name
-  region               = var.azure_region
-  name                 = "avtx-spoke-azure-${replace(lower(var.azure_region), " ", "-")}"
-  cidr                 = var.azure_cidr
+locals {
+  subnet_count = length(aviatrix_vpc.aviatrix_vpc_vnet.subnets[*].cidr)/2
+}
+
+
+resource "aviatrix_vpc" "aviatrix_vpc_vnet" {
+  cloud_type           = (var.cloud_type == "aws") ? 1 : 8
+  account_name         = (var.cloud_type == "aws") ? "aws-account" : "azure-account"
+  region               = var.cloud_region
+  name                 = "avtx-spoke-${var.spoke_gw_name}-net"
+  cidr                 = var.vnet_vpc_address_space
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
@@ -23,17 +28,15 @@ resource "aviatrix_vpc" "azure_vnet" {
 ####################################################################
 # Aviatrix Spoke GW
 
-resource "aviatrix_spoke_gateway" "azure-spoke-gw" {
-  cloud_type             = 8
-  vpc_reg                = var.azure_region
-  vpc_id                 = aviatrix_vpc.azure_vnet.vpc_id
-  account_name           = var.azure_account_name
-  gw_name                = var.gw_name
+resource "aviatrix_spoke_gateway" "avx-spoke-gw" {
+  cloud_type             = (var.cloud_type == "aws") ? 1 : 8
+  vpc_reg                = var.cloud_region
+  vpc_id                 = aviatrix_vpc.aviatrix_vpc_vnet.vpc_id
+  account_name           = (var.cloud_type == "aws") ? "aws-account" : "azure-account"
+  gw_name                = var.spoke_gw_name
   insane_mode            = var.hpe
-  gw_size                = var.hpe ? "Standard_D3_v2" : var.avtx_gw_size
-  ha_gw_size             = var.avtx_gw_ha ? (var.hpe ? "Standard_D3_v2" : var.avtx_gw_size) : null # min "Standard_D3_v2" for HPE
-  subnet                 = aviatrix_vpc.azure_vnet.subnets[0].cidr
-  ha_subnet              = var.avtx_gw_ha ? aviatrix_vpc.azure_vnet.subnets[1].cidr : null
+  gw_size                = (var.cloud_type == "aws") ? "t2.medium" : "Standard_B1ms"
+  subnet       = (var.cloud_type == "aws") ? aviatrix_vpc.spoke_net[count.index].subnets[local.subnet_count].cidr : aviatrix_vpc.spoke_net[count.index].subnets[0].cidr
   enable_active_mesh     = true
   manage_transit_gateway_attachment = false
 }
@@ -42,16 +45,16 @@ resource "aviatrix_spoke_gateway" "azure-spoke-gw" {
 # Spoke to Transit Attachment
 
 resource "aviatrix_spoke_transit_attachment" "spoke_transit_attachment" {
-  spoke_gw_name   = aviatrix_spoke_gateway.azure-spoke-gw.gw_name
-  transit_gw_name = var.avtx_transit_gw
+  spoke_gw_name   = aviatrix_spoke_gateway.avx-spoke-gw.gw_name
+  transit_gw_name = var.aviatrix_transit_gateway
 }
 
 #######################################################################
 # Spoke to Domain Association
 
-resource "aviatrix_segmentation_security_domain_association" "east_segmentation_security_domain_association" {
-  transit_gateway_name = var.avtx_transit_gw
-  security_domain_name = var.sec_domain_name
+resource "aviatrix_segmentation_security_domain_association" "segmentation_security_domain_association" {
+  transit_gateway_name = var.aviatrix_transit_gateway
+  security_domain_name = var.transit_segment
   attachment_name      = aviatrix_spoke_transit_attachment.spoke_transit_attachment.spoke_gw_name
 
 }
