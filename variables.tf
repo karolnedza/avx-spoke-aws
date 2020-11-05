ac7e91766b2e34e41248f2962d6bc70492acc18f
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-05 19:44:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,16 @@
 ##############################################
 # Variables 
 
-# variable "cloud_type" {}           
-# variable "cloud_region" {}
-# variable "spoke_gw_name" {}           
-# variable "vnet_vpc_address_space" {}   
-# variable "transit_segment" {}        
-# #variable "cloud_account_name" {} 
-# variable "aviatrix_transit_gateway" {}      
-
-
 variable "avtx_gw_ha" {default = false}
 variable "hpe" {default = false}
-# variable "ctrl_password" {}
-# variable "vcs_repository" {}
+
+variable "cloud_type" {}           
+variable "cloud_region" {}
+variable "spoke_gw_name" {}           
+variable "vnet_vpc_address_space" {}   
+variable "transit_segment" {}        
+
+variable "aviatrix_transit_gateway" {}      
+variable "ctrl_password" {}
+variable "vcs_repository" {}
 
