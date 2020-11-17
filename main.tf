85377b885848fdf580796072cda6d840b0cf6088
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-17 19:30:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-########## Provider 
-provider "aviatrix" {
-  username     = "admin"
-  password      = var.ctrl_password
-  controller_ip = "18.157.190.93"
-  version       = "2.17.0"
-}
-
-
 #######################################################################
 # VPC/Vnet
 #
