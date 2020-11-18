88804d97db08f5ea84984d0fb347cf437b3843aa
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-18 18:48:17 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,8 +6,9 @@ variable "hpe" {default = false}
 
 variable "cloud_type" {}           
 
-variable "cloud_region" {default = "us-east-1"}
+variable "aws_cloud_region" {default = "us-east-1"}
 
+variable "azure_cloud_region" {default = "East US"}
 
 variable "spoke_gw_name" {}           
 variable "vnet_vpc_address_space" {}   
