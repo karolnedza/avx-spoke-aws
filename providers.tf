128a21bc81593f17523446ee8484a42f73bbd0b3
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-11-20 11:40:36 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,7 +10,8 @@ provider "aviatrix" {
 
 provider "aws" {
   version    = "~> 2.0"
-  region     = var.aws_cloud_region
+ # region     = var.aws_cloud_region
+  region = var.cloud_region["${var.aviatrix_transit_gateway}"]
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
